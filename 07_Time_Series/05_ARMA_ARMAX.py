e5721d32eaf365bba889de213ce76bdeea822123
Carlo <carloghiglione98@gmail.com>
2022-07-03 22:29:16 +0200

    time series

--- 07_Time_Series/05_ARMA_ARMAX.py
+++ 07_Time_Series/05_ARMA_ARMAX.py
@@ -28,16 +28,30 @@ warnings.filterwarnings("ignore")
 df = pd.read_csv('data/others/hospital.csv',index_col='date', parse_dates=['date'])
 df = df.rename(columns={'wait_times_hrs':'y', 'nurse_count':'x'})
 
-fig, ax = plt.subplots(1,1)
-ax.plot(df['y'])
+train_split = 0.80
+y = df['y'].values
+X = df['x'].values
 
+n = len(y)
+n_train = int(n*train_split)
+n_test = n - n_train
+y_train, y_test = y[:n_train], y[n_train:]
+X_train, X_test = X[:n_train], X[n_train:]
+t_train, t_test = df.index[:n_train], df.index[n_train:]
+
+fig, ax = plt.subplots(1,1)
+ax.plot(t_train, y_train, label='Train')
+ax.plot(t_test, y_test, label='Test')
+ax.legend()
+ax.set_title('Data')
+plt.tight_layout()
 
 ######################################################################################
 # Test Stationarity (Augmented Dickey-Fuller Test)
 # H0: the series is random walk => non stationary
 # H1: the series is not random walk => may be stationary
 
-adf = adfuller(df['y'], regression='c')[1]
+adf = adfuller(y_train, regression='c')[1]
 print(f'Adf Test: pvalue={adf}')
 # I reject H0, the process may be stationary
 
@@ -45,12 +59,11 @@ print(f'Adf Test: pvalue={adf}')
 ######################################################################################
 # ARMA model selection
 fig, ax = plt.subplots(1,2)
-plot_acf(df['y'], ax=ax[0])    # cuts if process is MA(q)
-plot_pacf(df['y'], ax=ax[1])   # cuts if process is AR(q)
+plot_acf(y_train, ax=ax[0])    # cuts if process is MA(q)
+plot_pacf(y_train, ax=ax[1])   # cuts if process is AR(q)
 fig.tight_layout()
 # they both don't cut, I have an ARMA
 
-y = df['y'].values
 
 p = []
 q = []
@@ -61,7 +74,7 @@ ma_max = 5
 
 for ar_order in tqdm(range(ar_max)):
     for ma_order in range(ma_max):
-        model = ARIMA(y, order=(ar_order, 0, ma_order))
+        model = ARIMA(y_train, order=(ar_order, 0, ma_order))
         result = model.fit()
         
         p.append(ar_order)
@@ -85,20 +98,21 @@ print(bic_selected)
 ar_best = 2
 ma_best = 4
 
-mod_arma = ARIMA(y, order=(ar_best, 0, ma_best)).fit()
+mod_arma = ARIMA(y_train, order=(ar_best, 0, ma_best)).fit()
 
 mod_pred = mod_arma.get_prediction()
-y_hat = mod_pred.predicted_mean
+y_hat_train = mod_pred.predicted_mean
 y_hat_conf = mod_pred.conf_int()
 
 fig, ax = plt.subplots(1,1)
-ax.plot(df.index, y, label='Data')
-ax.plot(df.index, y_hat, color='red', label='Fitted')
-ax.fill_between(df.index, y_hat_conf[:,0], y_hat_conf[:,1], 
+ax.plot(t_train, y_train, label='Data')
+ax.plot(t_train, y_hat_train, color='red', label='Fitted')
+ax.fill_between(t_train, y_hat_conf[:,0], y_hat_conf[:,1], 
                 color='red', alpha=0.25, label='Conf 95%')
 ax.legend()
 ax.set_title(f'ARMA({ar_best}, {ma_best}), Model')
 
+########################
 # diagnostic
 print(mod_arma.summary())
 
@@ -107,12 +121,45 @@ plt.tight_layout()
 print(f'Durbin-Watson Test: {durbin_watson(mod_arma.resid)}')
 
 
+########################
+# Prediction
+y_hat_list = []
+res_list = []
+for i in range(n_test):
+    y_hat = mod_arma.forecast(steps=1)
+    res = y_test[i] - y_hat
+    y_hat_list.append(y_hat)
+    res_list.append(res)
+    mod_arma = mod_arma.append([y_test[i]], refit=False)
+
+res_list = np.array(res_list)
+rmse_test_arma = np.sqrt(np.mean(res_list**2))
+print(f'RMSE ARMA Test: {rmse_test_arma}')
+
+fig, ax = plt.subplots(1,1)
+ax.plot(y_test, label='data')
+ax.plot(y_hat_list, label='prediction')
+ax.legend()
+ax.set_title('ARMA Prediction')
+
+
+mod_pred = mod_arma.get_prediction()
+y_hat= mod_pred.predicted_mean
+y_hat_conf = mod_pred.conf_int()
+
+fig, ax = plt.subplots(1,1)
+ax.plot(df.index, y, label='Data')
+ax.plot(df.index, y_hat, color='red', label='Fitted')
+ax.fill_between(df.index, y_hat_conf[:,0], y_hat_conf[:,1], 
+                color='red', alpha=0.25, label='Conf 95%')
+ax.axvline(df.index[n_train], color='black', ls='--')
+ax.set_title('ARMA Model & Prediction')
+
+
 
 ###################################################################################
 # ARMAX model selection
 from sklearn.preprocessing import PolynomialFeatures
-y = df['y'].values
-x = df['x'].values
 # polynomial = PolynomialFeatures(degree=3, include_bias=False)
 # x = polynomial.fit_transform(x.reshape(-1,1))
 
@@ -125,7 +172,7 @@ ma_max = 5
 
 for ar_order in tqdm(range(ar_max)):
     for ma_order in range(ma_max):
-        model = ARIMA(y, order=(ar_order, 0, ma_order), exog=x)
+        model = ARIMA(y_train, order=(ar_order, 0, ma_order), exog=X_train)
         result = model.fit()
         
         p.append(ar_order)
@@ -148,10 +195,53 @@ print(bic_selected)
 # Fit ARMAX model
 ar_best = 1
 ma_best = 1
-mod_armax = ARIMA(y, order=(ar_best, 0, ma_best), exog=x).fit()
+mod_armax = ARIMA(y_train, order=(ar_best, 0, ma_best), exog=X_train).fit()
 
 mod_pred = mod_armax.get_prediction()
 y_hat = mod_pred.predicted_mean
+y_hat_conf = mod_pred.conf_int()
+
+fig, ax = plt.subplots(1,1)
+ax.plot(t_train, y_train, label='Data')
+ax.plot(t_train, y_hat, color='red', label='Fitted')
+ax.fill_between(t_train, y_hat_conf[:,0], y_hat_conf[:,1], 
+                color='red', alpha=0.25, label='Conf 95%')
+ax.legend()
+ax.set_title(f'ARMAX({ar_best}, {ma_best}), Model')
+
+####################
+# diagnostic
+print(mod_armax.summary())
+
+mod_armax.plot_diagnostics()
+plt.tight_layout()
+print(f'Durbin-Watson Test: {durbin_watson(mod_arma.resid)}')
+
+
+########################
+# Prediction
+y_hat_list = []
+res_list = []
+for i in range(n_test):
+    y_hat = mod_armax.forecast(steps=1, exog=X_test[i])
+    res = y_test[i] - y_hat
+    y_hat_list.append(y_hat)
+    res_list.append(res)
+    mod_armax = mod_armax.append([y_test[i]], exog=[X_test[i]], refit=False)
+
+res_list = np.array(res_list)
+rmse_test_armax = np.sqrt(np.mean(res_list**2))
+print(f'RMSE ARMAX Test: {rmse_test_arma}')
+
+fig, ax = plt.subplots(1,1)
+ax.plot(y_test, label='data')
+ax.plot(y_hat_list, label='prediction')
+ax.legend()
+ax.set_title('ARMAX Prediction')
+
+
+mod_pred = mod_armax.get_prediction()
+y_hat= mod_pred.predicted_mean
 y_hat_conf = mod_pred.conf_int()
 
 fig, ax = plt.subplots(1,1)
@@ -159,15 +249,8 @@ ax.plot(df.index, y, label='Data')
 ax.plot(df.index, y_hat, color='red', label='Fitted')
 ax.fill_between(df.index, y_hat_conf[:,0], y_hat_conf[:,1], 
                 color='red', alpha=0.25, label='Conf 95%')
-ax.legend()
-ax.set_title(f'ARMA({ar_best}, {ma_best}), Model')
-
-# diagnostic
-print(mod_armax.summary())
-
-mod_armax.plot_diagnostics()
-plt.tight_layout()
-print(f'Durbin-Watson Test: {durbin_watson(mod_arma.resid)}')
+ax.axvline(df.index[n_train], color='black', ls='--')
+ax.set_title('ARMAX Model & Prediction')
 
 
 ##################################################################################
@@ -177,8 +260,4 @@ armax_rmse = np.sqrt(np.mean(mod_armax.resid**2))
 
 print(f'ARMA RMSE: {arma_rmse}')
 print(f'ARMAX RMSE: {armax_rmse}')
-
-
-
-
 
--- 07_Time_Series/07_SARIMAX.py
+++ 07_Time_Series/07_SARIMAX.py
@@ -33,9 +33,21 @@ y_s = df['y']
 df = pd.read_csv('data/others/a10.csv', parse_dates=['date'], index_col='date')
 y_s = df['value']
 
+y = y_s.values
+train_split = 0.80
+
+n = len(y)
+n_train = int(n*train_split)
+n_test = n - n_train
+y_train, y_test = y[:n_train], y[n_train:]
+t_train, t_test = df.index[:n_train], df.index[n_train:]
+
 fig, ax = plt.subplots(1,1)
-ax.plot(y_s)
-fig.tight_layout()
+ax.plot(t_train, y_train, label='Train')
+ax.plot(t_test, y_test, label='Test')
+ax.legend()
+ax.set_title('Data')
+plt.tight_layout()
 
 
 ####################################################################################
@@ -62,8 +74,6 @@ p_best = 12
 
 #####################################################################################
 # SARIMA Model Selection
-y = y_s.values
-
 p = []
 q = []
 r = []
@@ -76,7 +86,7 @@ r_max = 2
 for ar_order in tqdm(range(ar_max)):
     for ma_order in range(ma_max):
         for r_order in range(r_max):
-            model = ARIMA(y, order=(ar_order, r_order, ma_order), seasonal_order=(1,1,1,p_best))
+            model = ARIMA(y_train, order=(ar_order, r_order, ma_order), seasonal_order=(1,1,1,p_best))
             result = model.fit()
         
             p.append(ar_order)
@@ -102,10 +112,54 @@ print(bic_selected)
 ar_best = 1
 ma_best = 1
 r_best = 1
-mod_arima = ARIMA(y, order=(ar_best, r_best, ma_best), seasonal_order=(1,1,1,p_best)).fit()
+mod_sarima = ARIMA(y_train, order=(ar_best, r_best, ma_best), seasonal_order=(1,1,1,p_best)).fit()
 
-mod_pred = mod_arima.get_prediction()
+mod_pred = mod_sarima.get_prediction()
 y_hat = mod_pred.predicted_mean
+y_hat_conf = mod_pred.conf_int()
+
+fig, ax = plt.subplots(1,1)
+ax.plot(t_train, y_train, label='Data')
+ax.plot(t_train, y_hat, color='red', label='Fitted')
+ax.fill_between(t_train[20:], y_hat_conf[20:,0], y_hat_conf[20:,1], 
+                color='red', alpha=0.25, label='Conf 95%')
+ax.legend()
+ax.set_title(f'ARIMA({ar_best}, {r_best}, {ma_best}), Model')
+fig.tight_layout()
+
+####################
+# diagnostic
+print(mod_sarima.summary())
+
+mod_sarima.plot_diagnostics()
+plt.tight_layout()
+print(f'Durbin-Watson Test: {durbin_watson(mod_sarima.resid)}')
+
+
+####################
+# Prediction
+y_hat_list = []
+res_list = []
+for i in range(n_test):
+    y_hat = mod_sarima.forecast(steps=1)
+    res = y_test[i] - y_hat
+    y_hat_list.append(y_hat)
+    res_list.append(res)
+    mod_sarima = mod_sarima.append([y_test[i]], refit=False)
+
+res_list = np.array(res_list)
+rmse_test_arma = np.sqrt(np.mean(res_list**2))
+print(f'RMSE SARIMA Test: {rmse_test_arma}')
+
+fig, ax = plt.subplots(1,1)
+ax.plot(y_test, label='data')
+ax.plot(y_hat_list, label='prediction')
+ax.legend()
+ax.set_title('SARIMA Prediction')
+
+
+mod_pred = mod_sarima.get_prediction()
+y_hat= mod_pred.predicted_mean
 y_hat_conf = mod_pred.conf_int()
 
 fig, ax = plt.subplots(1,1)
@@ -113,23 +167,7 @@ ax.plot(df.index, y, label='Data')
 ax.plot(df.index, y_hat, color='red', label='Fitted')
 ax.fill_between(df.index[20:], y_hat_conf[20:,0], y_hat_conf[20:,1], 
                 color='red', alpha=0.25, label='Conf 95%')
-ax.legend()
-ax.set_title(f'ARIMA({ar_best}, {r_best}, {ma_best}), Model')
-fig.tight_layout()
-
-# diagnostic
-print(mod_arima.summary())
-
-mod_arima.plot_diagnostics()
-plt.tight_layout()
-print(f'Durbin-Watson Test: {durbin_watson(mod_arima.resid)}')
-
-
-
-
-
-
-
+ax.axvline(df.index[n_train], color='black', ls='--')
 
 
 
